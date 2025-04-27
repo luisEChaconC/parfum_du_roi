f80e344b365cefdf11793dbd6d1b8b27e45d4ffd
ANTONY LEONARDO PICADO ALVARADO <antony.picado@ucr.ac.cr>
2025-04-26 21:34:54 -0600

    update the styles of TopBar

--- react-app - proyect/src/components/TopBar.tsx
+++ react-app - proyect/src/components/TopBar.tsx
@@ -5,18 +5,20 @@ function TopBar() {
   return (
     <nav className="navbar">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          <img src={logo} alt="Logo Perfumes" className="logo-img" />
-        </a>
-        <form className="d-flex" role="search">
+        <form className="d-flex search-form" role="search">
           <input
             className="form-control me-2"
             type="search"
             placeholder="Buscar perfumes..."
             aria-label="Buscar"
           />
-          <button className="btn btn-outline-success" type="submit">Buscar</button>
+          <button className="btn btn-outline-success" type="submit">
+            Buscar
+          </button>
         </form>
+        <a className="navbar-brand logo-container" href="#">
+          <img src={logo} alt="Logo Perfumes" className="logo-img" />
+        </a>
       </div>
     </nav>
   );
