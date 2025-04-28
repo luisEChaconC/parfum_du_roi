2357ab03254de32287539eedfe5cc3ec04181dd3
nacho-alesina05 <ignacio.alesina@xmartlabs.com>
2025-04-28 15:19:57 -0600

    feat: carrito working, bad UX/UI

--- react-app - proyect/src/components/ProductCard.tsx
+++ react-app - proyect/src/components/ProductCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./ProductCard.css";
-
+import { CarritoContext } from "../Context/Carrito/carrito";
 interface props {
   name: string;
   image: string;
@@ -8,13 +8,15 @@ interface props {
 }
 
 const ProductCard: React.FC<props> = ({ name, image, price }) => {
+  const { addToCart } = React.useContext(CarritoContext);
+  const handleAddToCart = () => {addToCart({ name, price: parseFloat(price), img: image })};
   return (
     <div className="card product-card">
       <img src={image} className="card-img-top" alt={name} />
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
         <p className="card-text">{price}</p>
-        <button id="btn" className="btn btn-primary">Comprar</button>
+        <button id="btn" className="btn btn-primary" onClick={handleAddToCart}>Agregar al Carrito</button>
       </div>
     </div>
   );
--- react-app - proyect/src/pages/Carrito/Carrito.tsx
+++ react-app - proyect/src/pages/Carrito/Carrito.tsx
@@ -1,8 +1,10 @@
 import { useContext } from "react"
 import { CarritoContext } from "../../Context/Carrito/carrito"
+import { Button } from "@mui/material"
 export const Carrito = () => {
-    const {totalPrice, amountOfProducts} = useContext(CarritoContext)
-    return (
+    const {totalPrice, amountOfProducts, clearCart} = useContext(CarritoContext)
+    return (<>
     <h1>este es el carrito, con precio ${totalPrice} y {amountOfProducts} productos</h1>
+    <Button onClick={clearCart}> Limpiar carrito</Button></>
 )
 }
