fde9c73967f92fa24eacf2d650aa30302452978e
Luis Esteban Chacon Chaves <luis.chaconchaves@ucr.ac.cr>
2025-06-23 22:50:22 -0600

    feat: create PresentationError class for error handling

---
+++ backend/src/presentation/errors/presentation.error.ts
@@ -0,0 +1,6 @@
+export abstract class PresentationError extends Error {
+  constructor(message: string, public readonly statusCode: number) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
