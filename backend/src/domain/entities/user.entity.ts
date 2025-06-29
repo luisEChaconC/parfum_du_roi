7d7ef885ca7f7189353798186cc84a4993c53826
Luis Esteban Chacon Chaves <luis.chaconchaves@ucr.ac.cr>
2025-06-29 17:27:30 -0600

    feat: add optional id property

--- backend/src/domain/entities/user.entity.ts
+++ backend/src/domain/entities/user.entity.ts
@@ -3,20 +3,24 @@ export class User {
     private _email: string,
     private _hashedPassword: string,
     private _fullName: string,
+    private _id?: string,
   ) {}
 
   // Getters
-
   public get email(): string {
     return this._email;
   }
-
+  
   public get hashedPassword(): string {
     return this._hashedPassword;
   }
 
   public get fullName(): string {
     return this._fullName;
+  }
+
+  public get id(): string | undefined {
+    return this._id;
   }
 }
 
