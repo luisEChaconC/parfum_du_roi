ae67e4d413b188ed6449349c8e2654fdd66bb789
Luis Esteban Chacon Chaves <luis.chaconchaves@ucr.ac.cr>
2025-06-28 20:00:56 -0600

    refactor: remove less common perfume concentration values from PerfumeConcentration enum

--- backend/src/domain/enums/perfume-concentration.enum.ts
+++ backend/src/domain/enums/perfume-concentration.enum.ts
@@ -3,9 +3,4 @@ export enum PerfumeConcentration {
   EAU_DE_PARFUM = 'Eau de Parfum',
   EAU_DE_TOILETTE = 'Eau de Toilette',
   EAU_DE_COLOGNE = 'Eau de Cologne',
-  EAU_FRAICHE = 'Eau Fraîche',
-  PERFUME_OIL = 'Perfume Oil',
-  BODY_MIST = 'Body Mist',
-  EAU_DE_TOILETTE_SPRAY = 'Eau de Toilette Spray',
-  EAU_DE_PARFUM_SPRAY = 'Eau de Parfum Spray',
 }
