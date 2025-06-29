c43bea11cbf1013b6c5990195caede7c389f7dda
Luis Esteban Chacon Chaves <luis.chaconchaves@ucr.ac.cr>
2025-06-28 19:59:32 -0600

    refactor: remove optional id property and its getter from Image and Note entities

--- backend/src/domain/entities/image.entity.ts
+++ backend/src/domain/entities/image.entity.ts
@@ -1,12 +1,7 @@
 export class Image {
   constructor(
     private _path: string,
-    private _id?: string,
   ) {}
-
-  public get id(): string | undefined {
-    return this._id;
-  }
 
   public get path(): string {
     return this._path;
--- backend/src/domain/entities/note.entity.ts
+++ backend/src/domain/entities/note.entity.ts
@@ -1,12 +1,7 @@
 export class Note {
   constructor(
     private _name: string,
-    private _id?: string,
   ) {}
-
-  public get id(): string | undefined {
-    return this._id;
-  }
 
   public get name(): string {
     return this._name;
