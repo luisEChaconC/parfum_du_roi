159c9c541060fcd7d7e0fc3dcd81a68ce0ed32f1
Luis Esteban Chacon Chaves <luis.chaconchaves@ucr.ac.cr>
2025-06-28 19:57:49 -0600

    refactor: rename execute method to executeAsync in getPerfumesByCategory use case interface for clarity

--- backend/src/application/ports/use-cases/get-perfume-by-category.use-case.interface.ts
+++ backend/src/application/ports/use-cases/get-perfume-by-category.use-case.interface.ts
@@ -2,5 +2,5 @@ import { PerfumeCategory } from "@domain/enums/perfume-category.enum";
 import { GetPerfumeByCategoryResponseDto } from "@application/dtos/perfume/get-perfume-by-category-response.dto";
 
 export interface IGetPerfumesByCategoryUseCase {
-  execute(category: PerfumeCategory): Promise<GetPerfumeByCategoryResponseDto[]>;
+  executeAsync(category: PerfumeCategory): Promise<GetPerfumeByCategoryResponseDto[]>;
 }
