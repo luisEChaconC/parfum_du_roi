7b6f8a60da327ed43b210b1d8de9a76b78bfecf3
Luis Esteban Chacon Chaves <luis.chaconchaves@ucr.ac.cr>
2025-06-29 09:00:45 -0600

    feat: add TypeORM repositories and use cases for perfumes and images to dependency injection container

--- backend/src/composition/dependencies.ts
+++ backend/src/composition/dependencies.ts
@@ -3,24 +3,40 @@ import { TYPES } from './types';
 
 // Infrastructure layer imports
 // Repositories
-import { PerfumeRepository } from '@repository/perfume.repository';
+import { TypeOrmPerfumeRepository } from '@repository/perfume.repository';
+import { TypeOrmImageRepository } from '@infrastructure/persistence/typeorm/repositories/image.repository';
+import { TypeOrmNoteRepository } from '@infrastructure/persistence/typeorm/repositories/note.repository';
+import { dataSource } from "@infrastructure/persistence/typeorm/data-source";
 
 // Application layer imports
 // Use cases
 import { GetPerfumesByCategoryUseCase } from '@use-case/perfume/get-perfumes-by-category.use-case';
+import { CreatePerfumeUseCase } from '@application/use-cases/perfume/create-perfume.use-case';
 
 // Presentation layer imports
 // Controllers
 import { PerfumeController } from '@controller/perfume.controller';
+import { TypeOrmProductRepository } from '@typeorm/repositories/product.repository';
+import { PerfumeRepositoryAdapter } from '@infrastructure/adapters/perfume.repository.adapter';
 
 export const registerDependencies = (container: Container): void => {
-  // Infrastructure layer dependencies
-  // Repositories
-  container.bind<PerfumeRepository>(TYPES.PerfumeRepository).to(PerfumeRepository).inRequestScope();
-
   // Application layer dependencies
   // Use cases
   container.bind<GetPerfumesByCategoryUseCase>(TYPES.GetPerfumesByCategoryUseCase).to(GetPerfumesByCategoryUseCase).inTransientScope();
+  container.bind<CreatePerfumeUseCase>(TYPES.CreatePerfumeUseCase).to(CreatePerfumeUseCase).inTransientScope();
+
+  // Infrastructure layer dependencies
+  // Adapters
+  container.bind<PerfumeRepositoryAdapter>(TYPES.PerfumeRepository).to(PerfumeRepositoryAdapter).inRequestScope();
+
+  // Repositories
+  container.bind<TypeOrmProductRepository>(TYPES.TypeOrmProductRepository).to(TypeOrmProductRepository).inRequestScope();
+  container.bind<TypeOrmImageRepository>(TYPES.TypeOrmImageRepository).to(TypeOrmImageRepository).inRequestScope();
+  container.bind<TypeOrmPerfumeRepository>(TYPES.TypeOrmPerfumeRepository).to(TypeOrmPerfumeRepository).inRequestScope();
+  container.bind<TypeOrmNoteRepository>(TYPES.TypeOrmNoteRepository).to(TypeOrmNoteRepository).inRequestScope();
+
+  // Data Sources
+  container.bind<typeof dataSource>(TYPES.DataSource).toConstantValue(dataSource);
 
   // Presentation layer dependencies
   // Controllers
--- backend/src/composition/types.ts
+++ backend/src/composition/types.ts
@@ -1,10 +1,23 @@
 export const TYPES = {
-  // Repositories
-  PerfumeRepository: Symbol.for('PerfumeRepository'),
-
+  // Application Layer
   // Use Cases
   GetPerfumesByCategoryUseCase: Symbol.for('GetPerfumesByCategoryUseCase'),
+  CreatePerfumeUseCase: Symbol.for('CreatePerfumeUseCase'),
 
+  // Repository Ports
+  PerfumeRepository: Symbol.for('PerfumeRepository'),
+
+  // Infrastructure Layer
+  // Repository Concrete Implementations
+  TypeOrmProductRepository: Symbol.for('TypeOrmProductRepository'),
+  TypeOrmPerfumeRepository: Symbol.for('TypeOrmPerfumeRepository'),
+  TypeOrmImageRepository: Symbol.for('TypeOrmImageRepository'),
+  TypeOrmNoteRepository: Symbol.for('TypeOrmNoteRepository'),
+
+  // Data Source
+  DataSource: Symbol.for('DataSource'),
+
+  // Presentation Layer
   // Controllers
   PerfumeController: Symbol.for('PerfumeController'),
 };
