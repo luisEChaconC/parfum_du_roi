e8203869894847e2d2d5403bdbad65997f67c076
Luis Esteban Chacon Chaves <luis.chaconchaves@ucr.ac.cr>
2025-06-29 17:20:19 -0600

    feat: add IUserRepository interface for user data operations

---
+++ backend/src/application/ports/repositories/user.repository.interface.ts
@@ -0,0 +1,7 @@
+import { User } from "@entity/user.entity";
+
+export interface IUserRepository {
+  save(user: User): Promise<User>;
+  findByEmail(email: string): Promise<User | null>;
+  existsByEmail(email: string): Promise<boolean>;
+}
