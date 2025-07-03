ed19200442a1bec2179b9c90e7d600a6a88a5b82
Luis Esteban Chacon Chaves <luis.chaconchaves@ucr.ac.cr>
2025-07-03 17:12:53 -0600

    feat(cart): add saveOrReplace method to CartRepositoryAdapter

--- backend/src/infrastructure/adapters/cart.repository.adapter.ts
+++ backend/src/infrastructure/adapters/cart.repository.adapter.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from "inversify";
 import { TYPES } from "@composition/types";
 import { ICartRepository } from "@port/repository/cart.repository.interface";
-import { TypeOrmCartRepository } from "@infrastructure/persistence/typeorm/repositories/cart.repository";
+import { TypeOrmCartRepository } from "@repository/cart.repository";
 import { Cart } from "@entity/cart.entity";
 import { CartMapper } from "@infrastructure/mapper/cart/cart.mapper";
 
@@ -11,7 +11,13 @@ export class CartRepositoryAdapter implements ICartRepository {
     @inject(TYPES.TypeOrmCartRepository) private readonly _cartRepository: TypeOrmCartRepository,
   ) {}
 
-  async findByUserId(userId: string): Promise<Cart> {
+  async saveOrReplace(cart: Cart): Promise<Cart> {
+    const cartModel = CartMapper.fromDomain(cart);
+    const savedCartModel = await this._cartRepository.saveOrReplace(cartModel);
+    return CartMapper.toDomain(savedCartModel);
+  }
+
+  async findByUserId(userId: string): Promise<Cart | null> {
     const cartModel = await this._cartRepository.findByUserId(userId);
     if (!cartModel) {
       return null;
