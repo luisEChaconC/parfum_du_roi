5a055659318e6e4dc765ee1a2f5c820de550ce12
Luis Esteban Chacon Chaves <luis.chaconchaves@ucr.ac.cr>
2025-07-03 04:22:41 -0600

    refactor: update IGetCartUseCase interface to include userId parameter in executeAsync method

--- backend/src/application/ports/use-cases/cart/get-cart.use-case.interface.ts
+++ backend/src/application/ports/use-cases/cart/get-cart.use-case.interface.ts
@@ -1,5 +1,5 @@
 import { GetCartResponseDto } from "@dto/cart/get-cart-response.dto";
 
 export interface IGetCartUseCase {
-  executeAsync(): Promise<GetCartResponseDto>;
+  executeAsync(userId: string): Promise<GetCartResponseDto>;
 }
