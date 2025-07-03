22b3aa50d5080fbcb67f316cfdd45acb98259fa8
Luis Esteban Chacon Chaves <luis.chaconchaves@ucr.ac.cr>
2025-07-03 17:08:01 -0600

    fix(cart): update findByUserId method to return Cart or null

--- backend/src/application/ports/repositories/cart.repository.interface.ts
+++ backend/src/application/ports/repositories/cart.repository.interface.ts
@@ -2,5 +2,5 @@ import { Cart } from "@entity/cart.entity";
 
 export interface ICartRepository {
   saveOrReplace(cart: Cart): Promise<Cart>;
-  findByUserId(userId: string): Promise<Cart>;
+  findByUserId(userId: string): Promise<Cart | null>;
 }
