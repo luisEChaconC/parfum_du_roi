426cf18dbcc7298686d13e5fdb158c3e3b5984ae
Luis Esteban Chacon Chaves <luis.chaconchaves@ucr.ac.cr>
2025-07-03 14:24:55 -0600

    feat(cart): add userId property to Cart entity

--- backend/src/domain/entities/cart.entity.ts
+++ backend/src/domain/entities/cart.entity.ts
@@ -2,8 +2,13 @@ import { CartItem } from './cart-item.entity';
 
 export class Cart {
   constructor(
+    private _userId: string,
     private _items: CartItem[],
   ) {}
+
+  public get userId(): string {
+    return this._userId;
+  }
 
   public get items(): CartItem[] {
     return this._items;
