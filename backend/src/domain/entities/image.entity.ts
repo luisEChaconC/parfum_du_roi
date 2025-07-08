a91257b476dc163469fb4bbd6e12d70709539eee
Luis Esteban Chacon Chaves <luis.chaconchaves@ucr.ac.cr>
2025-07-07 19:25:26 -0600

    feat: add ID generation to Image, Note, Product, and Perfume entities

--- backend/src/domain/entities/image.entity.ts
+++ backend/src/domain/entities/image.entity.ts
@@ -1,7 +1,14 @@
+import { randomUUID } from "crypto";
+
 export class Image {
   constructor(
     private _path: string,
+    private _id: string = randomUUID(),
   ) {}
+
+  public get id(): string {
+    return this._id;
+  }
 
   public get path(): string {
     return this._path;
