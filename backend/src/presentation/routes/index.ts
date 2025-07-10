27ffd943f1f176d885c8ed9a13ab5fab847b5142
Luis Esteban Chacon Chaves <luis.chaconchaves@ucr.ac.cr>
2025-07-10 14:01:26 -0600

    feat(routes): add cart routes to the main router

--- backend/src/presentation/routes/index.ts
+++ backend/src/presentation/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
-import { perfumeRoutes } from "./perfume.routes";
 import { authRoutes } from "./auth.routes";
 import { userRoutes } from "./user.routes";
+import { cartRoutes } from "./cart.routes";
+import { perfumeRoutes } from "./perfume.routes";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/user", userRoutes);
+router.use("/cart", cartRoutes);
 router.use("/perfume", perfumeRoutes);
 
 export { router as routes };
