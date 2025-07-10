8c132af6f4fa41ae380061148c712986e81fd076
Luis Esteban Chacon Chaves <luis.chaconchaves@ucr.ac.cr>
2025-07-10 09:03:34 -0600

    refactor(cart): update CartItem entity to use Product object

--- backend/src/domain/entities/cart-item.entity.ts
+++ backend/src/domain/entities/cart-item.entity.ts
@@ -1,28 +1,20 @@
+import { Product } from "./product.entity";
+
 export class CartItem {
   constructor(
-    private _productId: string,
-    private _productName: string,
-    private _priceAtTimeOfAdd: number,
+    private _product: Product,
     private _quantity: number,
   ) {}
 
-  public get productId(): string {
-    return this._productId;
+  public get product(): Product {
+    return this._product;
   }
-
-  public get productName(): string {
-    return this._productName;
-  }
-  
-    public get priceAtTimeOfAdd(): number {
-      return this._priceAtTimeOfAdd;
-    }
 
   public get quantity(): number {
     return this._quantity;
   }
 
   public getSubtotalPrice(): number {
-    return this._quantity * this._priceAtTimeOfAdd;
+    return this._quantity * this._product.price;
   }
 }
