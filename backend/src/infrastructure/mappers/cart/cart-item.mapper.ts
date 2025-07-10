f104994a3cd2047303ca08f153513cf265ae2b0f
Luis Esteban Chacon Chaves <luis.chaconchaves@ucr.ac.cr>
2025-07-10 09:11:03 -0600

    refactor(cart): integrate ProductMapper in CartItemMapper

--- backend/src/infrastructure/mappers/cart/cart-item.mapper.ts
+++ backend/src/infrastructure/mappers/cart/cart-item.mapper.ts
@@ -2,12 +2,12 @@ import { CartItem } from "@entity/cart-item.entity";
 import { CartItemModel } from "@model/cart-item.model";
 import { CartModel } from "@model/cart.model";
 import { ProductModel } from "@model/product.model";
+import { ProductMapper } from "../product.mapper";
 
 export class CartItemMapper {
 
   static fromDomain(cartItemEntity: CartItem, cartModel: CartModel): CartItemModel {
-    const productModel = new ProductModel();
-    productModel.stockKeepingUnit = cartItemEntity.productStockKeepingUnit;
+    const productModel = ProductMapper.fromDomain(cartItemEntity.product);
 
     const cartItemModel = new CartItemModel();
     cartItemModel.product = productModel;
@@ -18,11 +18,10 @@ export class CartItemMapper {
   }
 
   static toDomain(cartItemModel: CartItemModel): CartItem {
+    const productEntity = ProductMapper.toDomain(cartItemModel.product);
     return new CartItem(
-      cartItemModel.product.stockKeepingUnit,
-      cartItemModel.product.name,
+      productEntity,
       cartItemModel.quantity,
-      cartItemModel.product.price,
     );
   }
 }
